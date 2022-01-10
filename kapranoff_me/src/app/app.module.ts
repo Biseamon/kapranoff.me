d1294657614032bbfb296da06f356b577d6e9a67
mbc <marin.capranov@bbconsult.co.uk>
2022-01-10 18:53:21 +0000

    Adding new components and npm packages

--- kapranoff_me/src/app/app.module.ts
+++ kapranoff_me/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { NftSectionComponent } from './nft-section/nft-section.component';
+import { HeaderTextComponent } from './header-text/header-text.component';
+import { ContactFooterComponent } from './contact-footer/contact-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    JumbotronComponent,
+    NftSectionComponent,
+    HeaderTextComponent,
+    ContactFooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ kapranoff_me/src/app/nft-section/nft-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nft-section',
+  templateUrl: './nft-section.component.html',
+  styleUrls: ['./nft-section.component.scss']
+})
+export class NftSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
